081aa56c27c857cc4cd405d96c6441c19a535981
Alexandra Masi <masialexandra@gmail.com>
2025-03-04 09:54:54 +0100

    clean

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -10,36 +10,6 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
-    // public function handle(Request $request, Closure $next)
-    // {
-    //     try {
-    //         $user = JWTAuth::parseToken()->authenticate();
-            
-    //         if (!$user) {
-    //             return response()->json(['error' => 'Utilisateur non authentifié'], 401);
-    //         }
-
-    //         // Vérifier si un rôle est requis
-    //         if (!empty($roles)) {
-    //             $userRole = $user->Roles_id_role; // Vérifie le rôle de l'utilisateur connecté
-                
-    //             // Convertir les rôles acceptés en nombres si besoin
-    //             $roles = array_map(fn($role) => (int) $role, $roles);
-
-    //             if (!in_array($userRole, $roles)) {
-    //                 return response()->json(['error' => 'Accès interdit. Rôle insuffisant.'], 403);
-    //             }
-    //         }
-    //     } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-    //         return response()->json(['error' => 'Token expiré'], 401);
-    //     } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-    //         return response()->json(['error' => 'Token invalide'], 401);
-    //     } catch (\Exception $e) {
-    //         return response()->json(['error' => 'Problème avec le token: ' . $e->getMessage()], 401);
-    //     }
-
-    //     return $next($request);
-    //}
     public function handle(Request $request, Closure $next, ...$roles)
     {
         try {
